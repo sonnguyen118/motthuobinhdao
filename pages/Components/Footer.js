7341ec5cd67da88893dda8ff6a618404403bb3b7
Sơn Nguyễn Văn <sonnguyenprofessor@gmail.com>
2022-03-23 18:05:41 +0700

    fix footer new

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -4,170 +4,148 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="footer-section">
-      <div className="container ">
-        <div className="footer-cta pt-5 pb-5">
-          <div className="row">
-            <div className="col address-footer">
-              <div className="single-cta">
-                <i className="fas fa-map-marker-alt" />
-                <div className="cta-text">
-                  <h4 className="cta-text-h4">Find us</h4>
-                  <span>1010 Avenue, sw 54321, chandigarh</span>
-                </div>
-              </div>
+    <footer className="footer">
+      <div className="footer-block">
+        <div className="footer-block-1">
+          <div className="footer-block-1-col address-icon-footer">
+            <i className="footer-block-1-col-icon fas fa-map-marker-alt"></i>
+            <div className="footer-block-1-col-infor">
+              <p className="footer-block-1-col-infor-title">Địa Chỉ</p>
+              <p className="footer-block-1-col-infor-body">
+                Đô Lương, Đông Hưng, Thái Bình
+              </p>
             </div>
-            <div className="col">
-              <div className="single-cta phone-email-footer">
-                <i className="fas fa-phone" />
-                <div className="cta-text">
-                  <h4>Call us</h4>
-                  <span>9876543210 0</span>
-                </div>
-              </div>
+          </div>
+          <div className="footer-block-1-col phone-icon-footer">
+            <i className="footer-block-1-col-icon fas fa-phone-volume"></i>
+            <div className="footer-block-1-col-infor">
+              <p className="footer-block-1-col-infor-title">Liên Hệ</p>
+              <p className="footer-block-1-col-infor-body">0981.83.9980</p>
             </div>
-            <div className="col">
-              <div className="single-cta phone-email-footer mail-footer">
-                <i className="far fa-envelope-open" />
-                <div className="cta-text">
-                  <h4>Mail us</h4>
-                  <span>mail@info.com</span>
-                </div>
-              </div>
+          </div>
+          <div className="footer-block-1-col">
+            <i className="footer-block-1-col-icon fas fa-envelope-open"></i>
+            <div className="footer-block-1-col-infor">
+              <p className="footer-block-1-col-infor-title">Email</p>
+              <p className="footer-block-1-col-infor-body">
+                infor@motthuoubinhdao.com
+              </p>
             </div>
           </div>
         </div>
-        <div className="footer-content pt-5 pb-5">
-          <div className="row">
-            <div className="col">
-              <div className="footer-widget">
-                <div className="footer-logo">
-                  <a href="index.html">
-                    <img
-                      src="https://i.ibb.co/QDy827D/ak-logo.png"
-                      className="img-fluid"
-                      alt="logo"
-                    />
-                  </a>
-                </div>
-                <div className="footer-text">
-                  <p>
-                    Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
-                    sed do eiusmod tempor incididuntut consec tetur adipisicing
-                    elit,Lorem ipsum dolor sit amet.
-                  </p>
-                </div>
-                <div className="footer-social-icon">
-                  <span>Follow us</span>
-                  <a href="#">
-                    <i className="fab fa-facebook-f facebook-bg" />
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-twitter twitter-bg" />
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-google-plus-g google-bg" />
-                  </a>
-                </div>
+        <div className="footer-block-2">
+          <div className="footer-block-2-col">
+            <div className="footer-block-2-col-inforweb">
+              <div className="footer-block-2-col-inforweb-img">
+                <Image
+                  src={`/media/baner-logo-red-text-white.png`}
+                  // className="logo"
+                  // width={481}
+                  // height={82}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="description of image"
+                />
               </div>
+              {/* <p className="footer-block-2-col-inforweb-p">
+                Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed
+                do eiusmod tempor incididuntut consec tetur adipisicing
+                elit,Lorem ipsum dolor sit amet.
+              </p> */}
             </div>
-            <div className="col link-menu-footer">
-              <div className="footer-widget">
-                <div className="footer-widget-heading">
-                  <h3>Useful Links</h3>
-                </div>
-                <ul>
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">about</a>
-                  </li>
-                  <li>
-                    <a href="#">services</a>
-                  </li>
-                  <li>
-                    <a href="#">portfolio</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact</a>
-                  </li>
-                  <li>
-                    <a href="#">About us</a>
-                  </li>
-                  <li>
-                    <a href="#">Our Services</a>
-                  </li>
-                  <li>
-                    <a href="#">Expert Team</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact us</a>
-                  </li>
-                  <li>
-                    <a href="#">Latest News</a>
-                  </li>
-                </ul>
+            <div className="footer-block-2-col-social">
+              <div className="footer-block-2-col-social-p lien-ket-footer">
+                Theo Dõi Chúng Tôi Tại
               </div>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-facebook"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-instagram"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-youtube"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-twitter"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-skype"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-linkedin-in"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-pinterest"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <i className="footer-block-2-col-social-icon fab fa-github"></i>
+              </Link>
+              <Link href={"/"} passHref>
+                <div className="footer-block-2-col-social-icon footer-block-2-col-social-icon"></div>
+              </Link>
             </div>
-            <div className="col ">
-              <div className="footer-widget">
-                <div className="footer-widget-heading">
-                  <h3>Subscribe</h3>
-                </div>
-                <div className="footer-text mb-25">
-                  <p>
-                    Don’t miss to subscribe to our new feeds, kindly fill the
-                    form below.
-                  </p>
-                </div>
-                <div className="subscribe-form">
-                  <form action="#">
-                    <input type="text" placeholder="Email Address" />
-                    <button>
-                      <i className="fab fa-telegram-plane" />
-                    </button>
-                  </form>
+          </div>
+          <div className="footer-block-2-col lien-ket-footer">
+            <p className="footer-block-2-col-title">Liên Kết</p>
+            <ul className="footer-block-2-col-body">
+              <div>
+                <Link href={"/"} passHref>
+                  <li className="footer-block-2-col-body-li"> Trang Chủ</li>
+                </Link>
+                <Link href={"/"} passHref>
+                  <li className="footer-block-2-col-body-li"> Nghiên Cứu</li>
+                </Link>
+                <Link href={"/"} passHref>
+                  <li className="footer-block-2-col-body-li">Dự Án</li>
+                </Link>
+              </div>
+              <div>
+                <Link href={"/"} passHref>
+                  <li className="footer-block-2-col-body-li">Liên Hệ</li>
+                </Link>
+                <Link href={"/"} passHref>
+                  <li className="footer-block-2-col-body-li">Tuyển Dụng</li>
+                </Link>
+              </div>
+            </ul>
+          </div>
+          <div className="footer-block-2-col">
+            <p className="footer-block-2-col-title">
+              Hãy Liên Hệ Với Chúng Tôi
+            </p>
+            <div className="footer-block-2-col-form">
+              <p className="footer-block-2-col-form-p">
+                Đừng bỏ lỡ thời gian, hãy liên hệ ngay với chúng tôi, để biết
+                tiến độ dự án cùng những bài nghiên cứu lịch sử mới và chất
+                lượng nhất !
+              </p>
+              <div className="footer-block-2-col-form-sub">
+                <input
+                  className="footer-block-2-col-form-sub-input"
+                  type="text"
+                  name="email"
+                  placeholder="Email của bạn"
+                />
+                <div className="footer-block-2-col-form-sub-btn">
+                  <i className="footer-block-2-col-form-sub-btn-icon fab fa-telegram-plane"></i>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="copyright-area">
-        <div className="container">
-          <div className="row">
-            <div className="col text-center text-lg-left">
-              <div className="copyright-text">
-                <p>
-                  Copyright © 2018, All Right Reserved{" "}
-                  <a href="https://codepen.io/anupkumar92/">Anup</a>
-                </p>
-              </div>
-            </div>
-            <div className="col d-none d-lg-block text-right">
-              <div className="footer-menu">
-                <ul>
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">Terms</a>
-                  </li>
-                  <li>
-                    <a href="#">Privacy</a>
-                  </li>
-                  <li>
-                    <a href="#">Policy</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+
+      <div className="footer-copyright">
+        <p className="footer-copyright-p">
+          Copyright © 2021, All Right
+          <a
+            className="footer-copyright-p-a"
+            href="https://www.facebook.com/sonviolinandflute/"
+          >
+            Sơn Nguyễn
+          </a>
+        </p>
       </div>
     </footer>
   );
